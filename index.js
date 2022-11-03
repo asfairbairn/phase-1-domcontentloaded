ac3a7110f1ccfe42ff9aae37760d426ede15752a
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-11-03 12:08:31 -0500

    commit tree 4d27057e9aa55db9b887da2a6b147904fbe18e48

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+//document.addEventListener("DomContentLoaded", () => {
+    //document.querySelector("text")
+    //text.textContent = "This is really cool!"
+  //  console.log("This is really cool!")
+//});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("text")
+    text.textContent = "This is really cool!"
+});
+
+
+//const p = document.querySelector('#text');
+
+//document.addEventListener("DomContentLoaded", function() {
+  //  p.textContent = 'This is really cool!'
+//});
+
+
+
